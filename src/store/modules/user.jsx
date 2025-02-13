540c91c7ade81d784597649fa3b1e4d7f62826c0
xiayu <3389282692@qq.com>
2025-02-13 21:23:55 +0800

    token持久化

--- src/store/modules/user.jsx
+++ src/store/modules/user.jsx
@@ -3,11 +3,12 @@ import { request } from '@/utils'
 const userStore = createSlice({
   name: 'user',
   initialState: {
-    token: '',
+    token: localStorage.getItem('token_key') || '',
   },
   reducers: {
     setToken(state, action) {
       state.token = action.payload
+      localStorage.setItem('token_key', action.payload)
     },
   },
 })

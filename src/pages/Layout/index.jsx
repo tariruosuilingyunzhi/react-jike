104d578cb20e18894e75a933f007145874e63218
xiayu <3389282692@qq.com>
2025-02-13 22:35:15 +0800

    点击菜单跳转路由

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,9 +1,64 @@
-import { useEffect } from 'react'
-import { request } from '@/utils'
-const Layout = () => {
-  useEffect(() => {
-    request.get('/user/profile')
-  }, [])
-  return <div>我是layout</div>
+import { Layout, Menu, Popconfirm } from 'antd'
+import { HomeOutlined, DiffOutlined, EditOutlined, LogoutOutlined } from '@ant-design/icons'
+import { Outlet } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import './index.scss'
+
+const { Header, Sider } = Layout
+
+const items = [
+  {
+    label: '首页',
+    key: '/',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: '文章管理',
+    key: '/publish',
+    icon: <DiffOutlined />,
+  },
+  {
+    label: '创建文章',
+    key: '/article',
+    icon: <EditOutlined />,
+  },
+]
+
+const GeekLayout = () => {
+  const navigate = useNavigate()
+  const routerChange = e => {
+    const path = e.key
+    navigate(path)
+  }
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <div className="user-info">
+          <span className="user-name">柴柴老师</span>
+          <span className="user-logout">
+            <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
+              <LogoutOutlined /> 退出
+            </Popconfirm>
+          </span>
+        </div>
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            onClick={e => routerChange(e)}
+            mode="inline"
+            theme="dark"
+            defaultSelectedKeys={['1']}
+            items={items}
+            style={{ height: '100%', borderRight: 0 }}
+          ></Menu>
+        </Sider>
+        <Layout className="layout-content" style={{ padding: 20 }}>
+          <Outlet></Outlet>
+        </Layout>
+      </Layout>
+    </Layout>
+  )
 }
-export default Layout
+export default GeekLayout
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,10 +1,28 @@
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
+import Home from '@/pages/Home'
+import Publish from '@/pages/Publish'
+import Article from '@/pages/Article'
 import { createBrowserRouter } from 'react-router-dom'
+
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'publish',
+        element: <Publish />,
+      },
+      {
+        path: 'article',
+        element: <Article />,
+      },
+    ],
   },
   {
     path: '/login',

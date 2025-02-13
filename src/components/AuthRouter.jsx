5f922b94db2ca1733e6495a6e990d1b94abd0eb5
xiayu <3389282692@qq.com>
2025-02-13 22:11:08 +0800

    配置路由守卫

---
+++ src/components/AuthRouter.jsx
@@ -0,0 +1,11 @@
+import { getToken } from '@/utils'
+import { Navigate } from 'react-router-dom'
+const AuthRouter = ({ children }) => {
+  const token = getToken()
+  if (token) {
+    return <>{children}</>
+  } else {
+    // replace: 强制替换当前路由，而不是添加到历史记录中
+    return <Navigate to="/login" replace />
+  }
+}

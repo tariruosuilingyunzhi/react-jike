64d4c700b0ffd8fedd94f352b8254bc118b61273
xiayu <3389282692@qq.com>
2025-02-13 18:25:47 +0800

    登录表单校验实现

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -9,10 +9,32 @@ const Login = () => {
         <img className="login-logo" src={logo} alt="" />
         {/* 登录表单 */}
         <Form>
-          <Form.Item>
+          <Form.Item
+            validateTrigger="onBlur"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: '请输入手机号',
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '请输入正确的手机号',
+              },
+            ]}
+          >
             <Input size="large" placeholder="请输入手机号" />
           </Form.Item>
-          <Form.Item>
+          <Form.Item
+            validateTrigger="onBlur"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: '未输入验证码',
+              },
+            ]}
+          >
             <Input size="large" placeholder="请输入验证码" />
           </Form.Item>
           <Form.Item>

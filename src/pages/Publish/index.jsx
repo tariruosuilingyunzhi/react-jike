e5fdb1e80062ea11cbb093b8eda7bf4eb325d905
xiayu <3389282692@qq.com>
2025-02-14 20:47:56 +0800

    完成了封面的类型切换

--- src/pages/Publish/index.jsx
+++ src/pages/Publish/index.jsx
@@ -44,10 +44,17 @@ const Publish = () => {
   const onChange = value => {
     setImageList(value.fileList)
   }
+
+  // 切换封面类型
+  const [imageType, setImageType] = useState(0)
+  const onTypeChange = e => {
+    setImageType(e.target.value)
+  }
+
   return (
     <div className="publish">
       <Card title={<Breadcrumb items={[{ title: <Link to={'/'}>首页</Link> }, { title: '发布文章' }]} />}>
-        <Form onFinish={onFinish} labelCol={{ span: 4 }} wrapperCol={{ span: 16 }} initialValues={{ type: 1 }}>
+        <Form onFinish={onFinish} labelCol={{ span: 4 }} wrapperCol={{ span: 16 }} initialValues={{ type: 0 }}>
           <Form.Item label="标题" name="title" rules={[{ required: true, message: '请输入文章标题' }]}>
             <Input placeholder="请输入文章标题" style={{ width: 400 }} />
           </Form.Item>
@@ -62,23 +69,25 @@ const Publish = () => {
           </Form.Item>
           <Form.Item label="封面">
             <Form.Item name="type">
-              <Radio.Group>
+              <Radio.Group onChange={onTypeChange}>
                 <Radio value={1}>单图</Radio>
                 <Radio value={3}>三图</Radio>
                 <Radio value={0}>无图</Radio>
               </Radio.Group>
             </Form.Item>
-            <Upload
-              onChange={onChange}
-              name="image"
-              action={'http://geek.itheima.net/v1_0/upload'}
-              listType="picture-card"
-              showUploadList
-            >
-              <div style={{ marginTop: 8 }}>
-                <PlusOutlined />
-              </div>
-            </Upload>
+            {imageType !== 0 && (
+              <Upload
+                onChange={onChange}
+                name="image"
+                action={'http://geek.itheima.net/v1_0/upload'}
+                listType="picture-card"
+                showUploadList
+              >
+                <div style={{ marginTop: 8 }}>
+                  <PlusOutlined />
+                </div>
+              </Upload>
+            )}
           </Form.Item>
           <Form.Item label="内容" name="content" rules={[{ required: true, message: '请输入文章内容' }]}>
             <ReactQuill className="publish-quill" theme="snow" placeholder="请输入文章内容" />
